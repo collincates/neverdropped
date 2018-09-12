2b85163b2dcdf55d241670570bd5a20becfd7ce8
collincates <collin.cates@gmail.com>
2018-09-12 00:44:02 -0700

    Added Pipenv

--- scraper.py
+++ scraper.py
@@ -1,21 +1,57 @@
-
-city = input("Enter a city name: ")
-search_terms = input("Enter your search terms: ")
-
-#implement city lookup, with typos accounted for? maybe just a dict of all city names in CL)
-cities_dict = {"San Francisco": "sfbay",
-                "San Diego": "sandiego",
+ca_cities_dict = {
+                "Bakersfield": "bakersfield",
+                "Chico": "chico",
+                "Corcoran": "hanford",
+                "Fresno": "fresno",
+                "Gold Country": "goldcountry",
+                "Hanford": "hanford",
+                "Humboldt County": "humboldt",
+                "Imperial County": "imperial",
+                "Inland Empire": "inlandempire",
+                "Lake Tahoe": "reno",
                 "Los Angeles": "losangeles",
+                "Madera": "fresno",
+                "Mendocino County": "mendocino",
+                "Merced": "merced",
+                "Modesto": "modesto",
+                "Monterey Bay": "monterey",
+                "Orange County": "orangecounty",
+                "Palm Springs": "palmsprings",
+                "Redding": "redding",
+                "Reno": "reno",
+                "Riverside": "inlandempire",
+                "Sacramento": "sacramento",
+                "San Bernardino": "inlandempire",
+                "San Diego": "sandiego",
+                "San Francisco": "sfbay",
+                "San Luis Obispo": "slo",
+                "Santa Barbara": "santabarbara",
+                "Santa Maria": "santamaria",
+                "Siskiyou": "siskiyou",
+                "Stockton": "stockton",
+                "Susanville": "susanville",
+                "Sutter": "yubasutter",
+                "Tulare": "visalia",
+                "Ventura": "ventura",
+                "Visalia": "visalia",
+                "Yuba": "yubasutter",
 }
 
+def make_url(city, search_terms):
+#city = input("Enter a city name: ")
+#search_terms = input("Enter your search terms: ")
 
-full_URL = f"https://{cities_dict[city]}.craigslist.org/search/mcy?format=rss&query=\
-{'+'.join([term for term in search_terms.lower().split()])}"
+#implement city lookup of all city names in CL. How to handle typos? Eventually this will not be an input() scenario.
+#   https://geo.craigslist.org/iso/us/ca
 
-print(full_URL)
-# get search terms, make this a parameter of the module. e.g. "scrape("BMW 250 Series")"
-# split search terms on whitespace, (optional) assign each term to a category (make, model, etc.)
-#
+
+    return f"https://{city}.craigslist.org/search/mcy?format=rss&query={'+'.join([term for term in search_terms.lower().split()])}"
+
+
+# make search terms a parameter of the module. e.g. "scrape("BMW 250 Series")"
+
+# (optional) assign each term to a category (make, model, etc.)
+
 # collect CL RSS feed using search terms
 # store feed into memory
 #
@@ -36,3 +72,7 @@ print(full_URL)
 #
 # scraper module returns set() of listings in the {current: x} category
 #     (other categories are still added to the DB file, etc. as above)
+
+if __name__ == "__main__":
+    for city_abbrev in ca_cities_dict:
+        print(make_url(ca_cities_dict[city_abbrev], "Kawasaki Ninja"))
