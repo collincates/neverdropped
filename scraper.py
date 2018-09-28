5db558e360770b8c6e5a1f7b3f57774b44b13f63
collincates <collin.cates@gmail.com>
2018-09-27 22:13:24 -0700

    Added parse and get rss urls

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import requests
-
+from bs4 import BeautifulSoup
+import time
 
 ca_cities_dict = {
                 "Bakersfield": "bakersfield",
@@ -40,7 +41,7 @@ ca_cities_dict = {
                 "Yuba": "yubasutter",
 }
 
-def make_url(city, search_terms):
+def make_rss(city, search_terms):
 #city = input("Enter a city name: ")
 #search_terms = input("Enter your search terms: ")
 
@@ -85,7 +86,7 @@ def make_url(city, search_terms):
                     "Yuba": "yubasutter",
     }
 
-    return f"https://{ca_cities_dict[city]}.craigslist.org/search/mcy?format=rss&query={'+'.join([term for term in search_terms.lower().split()])}"
+    return f"https://{ca_cities_dict[city_abbrev]}.craigslist.org/search/mcy?format=rss&query={'+'.join([term for term in search_terms.lower().split()])}"
 
 
 # make search terms a parameter of the module. e.g. "scrape("BMW 250 Series")"
@@ -93,13 +94,71 @@ def make_url(city, search_terms):
 # (optional) assign each term to a category (make, model, etc.)
 
 
-# collect CL RSS feed using search terms
-# store feed into memory
-#
+
+# store feed into memory/database
+
+
 # return RSS feed entries that contain ALL of the search terms, and only ALL terms
+def get_urls_from_rss(rss_feed_url):
+    "Returns a list of posting URLs from a CL RSS feed"
+    cl_rss = requests.get(rss_feed_url)
+    soup = BeautifulSoup(cl_rss.text, 'html.parser')
+    urls = [soup.find('rdf:li')['rdf:resource'] \
+                for list_item in soup.find_all('rdf:li')]
+    print('got list of urls')
+    return urls_list
+
+
+def parse(posting_url):
+    # data = dict()
+    #listing title, the location, price, the body text, the images at highest resolution, and the link to the original post.
+    cl_posting = requests.get(posting_url)
+    soup = BeautifulSoup(cl_posting.text, 'html.parser')
+
+    posting_id = soup.find('div', class_='postinginfos') \
+                            .find('p', class_='postinginfo') \
+                            .getText().lstrip("post id: ")
+
+    title = soup.find(id='titletextonly').text
+
+    try:
+        price = soup.find(class_='price').text
+    except AttributeError:
+        price = "null"
+
+    try:
+        location = soup.find('small').text.strip(' () ')
+    except AttributeError:
+        location = "null"
+
+    try:
+        body_text = soup.find(id='postingbody').text #contents[2::]
+    except AttributeError:
+        body_text = "null"
+
+    when_posted = soup.find('p', id='display-date') \
+                            .find('time', class_='date timeago')['datetime']
+
+    #images = soup.find_all('images')
+    orig_url = posting_url
+
+    return {
+    'posting_id': posting_id,
+    'title': title,
+    'price': price,
+    'location': location,
+    'body_text': body_text,
+    'when_posted': when_posted,
+    'original url': orig_url
+    }
+
+def runit(rss_feed_url):
+    url_list = get_urls_from_rss(rss_feed_url)
+    for url in url_list:
+        print(parse(url))
+        time.sleep(15)
 #
 # check feed repetition by comparing URLs, maybe something else is a better identifier
-#
 # any new items are added to the main DB set
 # any repeated items are sent to a discarded set
 # any items that do not appear in the RSS feed and haven't been sent to the main DB
@@ -116,4 +175,7 @@ def make_url(city, search_terms):
 
 if __name__ == "__main__":
     for city_abbrev in ca_cities_dict:
-        print(make_url(ca_cities_dict[city_abbrev], "Kawasaki Ninja"))
+        # collect CL RSS feed using search terms
+        link = make_rss(ca_cities_dict[city_abbrev], "Kawasaki Ninja")
+        print(link)
+        print(type(link))
