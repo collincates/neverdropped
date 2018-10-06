295133dd6f8104fa0c758fa04b1cd27ce0a025c5
collincates <collin.cates@gmail.com>
2018-10-05 22:24:53 -0700

    duplicate cl_id's can't be added now

--- wordpresspost.py
+++ wordpresspost.py
@@ -1,19 +1,34 @@
 from wordpress_xmlrpc import Client, WordPressPost
 from wordpress_xmlrpc.methods.posts import NewPost
+from wordpress_xmlrpc.methods import taxonomies
+
+
 # from wordpress_xmlrpc.methods.users import GetUserInfo
 
 wordpress_url = 'http://localhost:8888/motocl/xmlrpc.php'
 username = 'collin'
-password = 'lUJijCgtkRZQ&gY4fz'
+password = '0wlL prfE pqjU 5ru0 OREt oa3V' #base64 encoded version of my pass
+
 
 wp = Client(wordpress_url, username, password)
 
-def newpost(cl_dictionary):
-    post = WordPressPost()
-    post.title = cl_dictionary['title']
-    post.content = cl_dictionary['body_text']
-    post.post_status = 'publish'
-    wp.call(NewPost(post))
+def new_wp_post(posting_data):
+    # check if a cl_id already exists in the WP DB. If so, pass.
+    tag_list = [tag.name for tag in [entry for entry in wp.call(taxonomies.GetTerms('post_tag'))]]
+    if posting_data['cl_id'] in tag_list:
+        print('{} already in database'.format(posting_data['cl_id']))
+        pass
+    else:
+        post = WordPressPost()
+        post.title = posting_data['title']
+        post.content = posting_data['body_text']
+        print('{} this work?'.format(posting_data['cl_id']))
+        post.terms_names = {
+            'post_tag': [posting_data['cl_id']]
+        }
+        print('this work?')
+        post.post_status = posting_data['post_status']
+        wp.call(NewPost(post))
 
 
         # 'posting_id': posting_id,
@@ -23,3 +38,5 @@ def newpost(cl_dictionary):
         # 'body_text': body_text,
         # 'when_posted': when_posted,
         # 'original url': orig_url
+
+new_wp_post({'title': 'new one', 'body_text': 'this isthe body text', 'post_status': 'publish', 'cl_id': '012398'})
