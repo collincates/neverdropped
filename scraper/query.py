6e742752a36ae2cfcd95c543092c24c9b1d6ca75
collincates <collin.cates@gmail.com>
2018-11-18 13:55:29 -0800

    Using different oauth technique

--- scraper/query.py
+++ scraper/query.py
@@ -2,47 +2,59 @@ import json
 import os
 from httplib2 import Http
 from googleapiclient.discovery import build
-from oauth2client import file, client, tools
-
+# from oauth2client import file, client, tools
+from google.oauth2.credentials import Credentials
 QUERY_FILE_ID = os.environ.get('QUERY_FILE_ID')
 
 def get_queries():
-    store = file.Storage('token.json')
-    creds = store.get()
+    # store = file.Storage('token.json')
+    # # creds = store.get()
+    # creds = None
 
-   # creds = client.Credentials(
-    #     filename=None,
-    #     scope=SCOPES,
-    #     token_uri="https://www.googleapis.com/oauth2/v3/token",
-    #     refresh_token="1/fsmi-AZajUNN-OvFd1K5ijkfQ2M-6SLw3Q1JkldQA0I"
-    # )
 
-    if not creds or creds.invalid:
 
-        CLIENT_ID = os.environ.get('CLIENT_ID')
-        CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
-        REDIRECT_URI = os.environ.get('REDIRECT_URI')
-        AUTH_URI = os.environ.get('AUTH_URI')
-        TOKEN_URI = os.environ.get('TOKEN_URI')
-        REVOKE_URI = os.environ.get('REVOKE_URI')
+    # if not creds or creds.invalid:
 
-        SCOPES = 'https://www.googleapis.com/auth/drive.readonly'
+    CLIENT_ID = os.environ.get('CLIENT_ID')
+    CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
+    REDIRECT_URI = os.environ.get('REDIRECT_URI')
+    AUTH_URI = os.environ.get('AUTH_URI')
+    TOKEN_URI = os.environ.get('TOKEN_URI')
+    REVOKE_URI = os.environ.get('REVOKE_URI')
 
-        flow = client.OAuth2WebServerFlow(
-                client_id=CLIENT_ID,
-                client_secret=CLIENT_SECRET,
-                redirect_uri=REDIRECT_URI,
-                auth_uri = AUTH_URI,
-                token_uri = TOKEN_URI,
-                revoke_uri = REVOKE_URI,
-                scope=SCOPES,
-                access_type='offline',
-                prompt='consent'
-                )
+    SCOPES = 'https://www.googleapis.com/auth/drive.readonly'
 
-        creds = tools.run_flow(flow, store)
 
-    service = build('drive', 'v3', http=creds.authorize(Http()))
+    # flow = client.OAuth2WebServerFlow(
+    #         client_id=CLIENT_ID,
+    #         client_secret=CLIENT_SECRET,
+    #         redirect_uri=REDIRECT_URI,
+    #         auth_uri = AUTH_URI,
+    #         token_uri = TOKEN_URI,
+    #         revoke_uri = REVOKE_URI,
+    #         scope=SCOPES,
+    #         access_type='offline',
+    #         prompt='consent'
+    #         )
+    #
+    # creds = tools.run_flow(flow, None)
+
+
+    credentials = Credentials(
+         token=None,
+         client_id=CLIENT_ID,
+         client_secret=CLIENT_SECRET,
+         # redirect_uri=REDIRECT_URI,
+         # auth_uri = AUTH_URI,
+         token_uri = TOKEN_URI,
+         # revoke_uri = REVOKE_URI,
+         scopes=SCOPES,
+         # access_type='offline',
+         # prompt='consent',
+         refresh_token="1/fsmi-AZajUNN-OvFd1K5ijkfQ2M-6SLw3Q1JkldQA0I"
+    )
+    print(type(credentials))
+    service = build('drive', 'v3', credentials=credentials)#http=credentials.authorize(Http()))
 
     # results = service.files().get_media(fileId=file_id).execute()
     results = service.files().export(fileId=QUERY_FILE_ID, mimeType='text/plain').execute()
